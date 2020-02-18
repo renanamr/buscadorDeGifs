d3cccfa887c1bb73cf734f8c7390b321d721d54f
ArthurAlvesIF <arthurantonio45@gmail.com>
2020-02-18 15:36:10 +0000

    Novo

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(home_page());
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'dart:convert';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'gif_page.dart';
+
+const urlTop =
+    "https://api.giphy.com/v1/gifs/trending?api_key=BGrSLJVE8IyRjGCIYPW5QlJ6e3A3afEJ&limit=20&rating=G";
+
+class home_page extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        hintColor: Colors.white,
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> getGifs() async {
+    http.Response response;
+    if (_search == null)
+      response = await http.get(urlTop);
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=BGrSLJVE8IyRjGCIYPW5QlJ6e3A3afEJ&q=$_search&limit=19&offset=$_offset&rating=G&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: Image.network(
+              "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Pesquise Aqui!",
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 18),
+                textAlign: TextAlign.center,
+                onSubmitted: (texto) {
+                  setState(() {
+                    _search = texto;
+                    _offset = 0;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: getGifs(),
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.none:
+                          return Container(
+                            width: 20.0,
+                            height: 20.0,
+                            child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                              strokeWidth: 5,
+                            ),
+                          );
+                          break;
+                        case ConnectionState.waiting:
+                          return Container(
+                            width: 20.0,
+                            height: 20.0,
+                            child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                              strokeWidth: 5.0,
+                            ),
+                          );
+                          break;
+                        default:
+                          if (snapshot.hasError)
+                            return Container();
+                          else
+                            return _createGifTable(context, snapshot);
+                      }
+                    }))
+          ],
+        ));
+  }
+
+  int _getCount(List data) {
+    if (_search == null || _search.isEmpty) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length)
+            return GestureDetector(
+              child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                    fit: BoxFit.cover,
+                    height: 300.0,
+                  ),
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              },
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context){
+                  return GifPage(snapshot.data["data"][index]);
+                }));
+              },
+            );
+          else
+            return GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.add,
+                    size: 70,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Carregar Mais",
+                    style: TextStyle(color: Colors.white, fontSize: 22),
+                  )
+                ],
+              ),
+              onTap: (){
+                setState(() {
+                  _offset += 19;
+                });
+              },
+            );
+        });
+  }
+}
